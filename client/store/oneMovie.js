866ceab6da8c9fd8629fe48b089aa7a79206560e
Sarah Sheppard <sarahmariesheppard@gmail.com>
2021-09-30 18:28:23 -0400

    admin: organizing thoughts/next steps

---
+++ client/store/oneMovie.js
@@ -0,0 +1,2 @@
+//Find one movie -- uses the direct movie URL format: https://api.themoviedb.org/3/movie/157336?api_key=964c3023860303f4cb3d17fa3814e4db
+
---
+++ server/api/movies.js
@@ -0,0 +1,16 @@
+const router = require('express').Router()
+const { models: { Movie }} = require('../db')
+
+//GET all of a User's Movies - findAll where userId
+
+//POST add a Movie to their Watched List -- where userId, movieId? Where does the shelf come into play?
+
+//PUT update a Movie on their Watched List (Ratings, Date Watched, Notes) -- where userId, movieId?
+
+//DELETE remove a Movie on their Watched List -- where userId, movieId?
+
+
+
+
+
+module.exports=router
