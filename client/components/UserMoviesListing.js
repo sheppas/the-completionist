a4a9a7a85bb9504548cd95845184969a5198e923
Sarah Sheppard <sarahmariesheppard@gmail.com>
2021-10-03 15:54:18 -0400

    style: user movie listing

--- client/components/UserMoviesListing.js
+++ client/components/UserMoviesListing.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { deleteUserMovie, fetchUserMovies } from "../store/userMovies";
 import { Link } from "react-router-dom";
 import { fetchMovieTMDB } from "../store/oneMovie";
+import dateFormat from "dateformat";
 
 class UserMoviesListing extends React.Component {
   constructor(props) {
@@ -36,30 +37,39 @@ class UserMoviesListing extends React.Component {
 
     return (
       <div>
-        <h2>Your Movies</h2>
-        {yourMovies.map((movie) => (
-          <div key={movie.id} className='individual-users-movie'>
-            <img
-              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-              width='150'
-              height='225'
-            />
-            <p>{movie.title}</p>
-            <p>Rating: {movie.rating}</p>
-            <p>Watched: {movie.watched_date}</p>
-            <p>Your Notes: {movie.notes}</p>
-            <Link to={`/edit-movie/${movie.id}`}>
-            <button>Edit Movie</button>
-            </Link>
-            <button
-              onClick={() => {
-                this.props.deleteMovie(userId, movie);
-              }}
-            >
-              Delete Movie
-            </button>
+        <hr />
+        <div>
+          <h2>Your Movies</h2>
+          <div className='listed-user-movies'>
+            {yourMovies.map((movie) => (
+              <div key={movie.id} className='individual-user-movie'>
+                <img
+                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                  width='150'
+                  height='225'
+                />
+                <div id='indiv-user-movie-text'>
+                  <h3>{movie.title}</h3>
+                  <p>Rating: {movie.rating}</p>
+                  <p>Watched: {dateFormat(movie.watched_date, "mmmm d, yyyy")}</p>
+                  <p id="user-notes">Your Notes: {movie.notes}</p>
+                  <div id='user-movie-button'>
+                    <Link to={`/edit-movie/${movie.id}`}>
+                      <button>Edit Movie</button>
+                    </Link>
+                    <button
+                      onClick={() => {
+                        this.props.deleteMovie(userId, movie);
+                      }}
+                    >
+                      Delete Movie
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
     );
   }
